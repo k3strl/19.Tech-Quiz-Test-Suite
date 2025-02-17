43ee6f825bfb0b0b38797442e18e67ccbb7301df
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2025-02-17 15:02:25 -0600

    sending it before everything breaks again

--- cypress/component/Quiz.cy.jsx
+++ cypress/component/Quiz.cy.jsx
@@ -1,6 +1,12 @@
 import Quiz from '../../client/src/components/Quiz'
 
 describe('Quiz', () => {
+
+  it('mounts and displays the "start quiz" button', () => {
+    cy.mount(<Quiz />);
+    cy.get('button').contains('Start Quiz').should("be.visible");
+  });
+
   it('start the quiz when the start button is clicked', () => {
     cy.mount(<Quiz />)
     cy.get('.btn').contains('Start Quiz').click();
--- cypress/e2e/quiz.cy.ts
+++ cypress/e2e/quiz.cy.ts
@@ -6,16 +6,8 @@ describe('Tech Quiz e2e tests', () => {
   it("should show a button with Start Quiz on page load", () => {
     cy.get("button").contains("Start Quiz").should("be.visible");
   });
-  
-  // it('should start quiz when button is clicked', () => {
-  //   cy.get('button').contains('Start Quiz').click();
-  //   cy.get(".card").should("be.visible").and('contain', "1");
-  //   cy.get(".card").should("be.visible").and('contain', "2");
-  //   cy.get(".card").should("be.visible").and('contain', "3");
-  //   cy.get(".card").should("be.visible").and('contain', "4");
-  // });
     
-  it('should answer all 10 questions', () => {
+  it('should choose the first answer for all 10 questions', () => {
     cy.get('button').contains('Start Quiz').click();
     for (let i = 0; i < 10; i++) {
       cy.get('button').contains('1').click();
@@ -24,4 +16,5 @@ describe('Tech Quiz e2e tests', () => {
     cy.contains('Your score:').should('be.visible');
     cy.get('button').contains('Take New Quiz').should('be.visible');
   });
+  // cy.get(".alert-success").should("be.visible").and('contain', 'Your score');
 })
