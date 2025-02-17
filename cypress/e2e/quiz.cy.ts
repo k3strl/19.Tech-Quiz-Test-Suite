7df72dcdffdc89abb92129aae61a853a22294f6d
Nic Blaize <114842389+k3strl@users.noreply.github.com>
2025-02-17 14:32:49 -0600

    committing before it breaks again

--- cypress/e2e/quiz.cy.ts
+++ cypress/e2e/quiz.cy.ts
@@ -1,27 +1,27 @@
 describe('Tech Quiz e2e tests', () => {
   beforeEach(() => {
-    cy.visit('http://127.0.0.1:3001')
+    cy.visit('http://127.0.0.1:3000')
   })
   
   it("should show a button with Start Quiz on page load", () => {
     cy.get("button").contains("Start Quiz").should("be.visible");
   });
   
-  
   // it('should start quiz when button is clicked', () => {
-    //   cy.get('button').contains('Start Quiz').click();
-    //   cy.get(".card").should("be.visible").and('contain', "1");
-    //   cy.get(".card").should("be.visible").and('contain', "2");
-    //   cy.get(".card").should("be.visible").and('contain', "3");
-    //   cy.get(".card").should("be.visible").and('contain', "4");
+  //   cy.get('button').contains('Start Quiz').click();
+  //   cy.get(".card").should("be.visible").and('contain', "1");
+  //   cy.get(".card").should("be.visible").and('contain', "2");
+  //   cy.get(".card").should("be.visible").and('contain', "3");
+  //   cy.get(".card").should("be.visible").and('contain', "4");
+  // });
     
-it('should answer all 10 questions', () => {
-  for (let i = 0; i < 10; i++) {
-     // cy.get('h2').should('be.visible')
-    cy.get('button').contains('1').click()
+  it('should answer all 10 questions', () => {
+    cy.get('button').contains('Start Quiz').click();
+    for (let i = 0; i < 10; i++) {
+      cy.get('button').contains('1').click();
     }
-    cy.contains('Quiz Completed').should('be.visible')
-    cy.contains('Your score:').should('be.visible')
-    cy.get('button').contains('Take New Quiz').should('be.visible')
-  })   
+    cy.contains('Quiz Completed').should('be.visible');
+    cy.contains('Your score:').should('be.visible');
+    cy.get('button').contains('Take New Quiz').should('be.visible');
+  });
 })
